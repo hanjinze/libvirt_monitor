439ba7e519ed3e1280a55920214a6fd12f533e93
halfss <ljvsss@gmail.com>
2012-07-21 13:29:05 +0800

    取消守护进程

--- monitor_vm.py
+++ monitor_vm.py
@@ -14,16 +14,11 @@ queue=Queue.Queue()
 MAXTHREAD=5
 log_dir=sys.path[0]
 
-uri='qemu+tcp:///system'
+uri='qemu:///system'
 def get_node_info():
 	global MAXTHREAD
 	conn=libvirt.open(uri)
 	doms=conn.listDomainsID()
-	if doms==[]:
-		while True:
-			doms=conn.listDomainsID()
-			if doms==[]:
-				time.sleep(60)
 	for dom_id in doms:
 		queue.put(dom_id)
 		class get_dom_info(threading.Thread):
@@ -47,7 +42,7 @@ def get_node_info():
 						for block in disks:
 							block_status0[block]=dom.blockStats(block)
 						for nic in nics:
-							net_status0[nic]=[os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
+							net_status0[nic]=[os.popen("cat /proc/net/dev |grep -w '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
 						Lstart_time=time.time()*1000000000
 						Dstart_time=dom.info()[4]
 						time.sleep(2)
@@ -56,12 +51,12 @@ def get_node_info():
 						for block in disks:
 							block_status1[block]=dom.blockStats(block)
 						for nic in nics:
-							net_status1[nic]=[os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
+							net_status1[nic]=[os.popen("cat /proc/net/dev |grep -w  '"+nic+"' |awk '{print $10}'").readlines()[0][:-1],os.popen("cat /proc/net/dev |grep '"+nic+"' |awk '{print $2}'").readlines()[0][:-1]]
 						for block in get_devices(dom,"devices/disk/source","file"):
 							block_info.append(dom.blockInfo(block,0))
 						for nic in nics:
 							try:
-								nic_status+=nic+"-"+str(int(net_status1[nic][1]))+"-"+str((int(net_status1[nic][1])-int(net_status0[nic][1]))*4)+"-"+str(int(net_status1[nic][0]))+"-"+str((int(net_status1[nic][0])-int(net_status0[nic][0]))*4)+"::"
+								nic_status+=nic+"-"+str(int(net_status1[nic][1]))+"-"+str((int(net_status1[nic][1])-int(net_status0[nic][1]))/2048)+"-"+str(int(net_status1[nic][0]))+"-"+str((int(net_status1[nic][0])-int(net_status0[nic][0]))/2048)+"::"
 							except:
 								nic_stauus=''
 						i=0
@@ -74,10 +69,10 @@ def get_node_info():
 						if dom_info[0]==1:
 							vm_status='active'
 						memstatus=os.popen("awk /VmRSS/'{print $2}' /proc/`ps aux | grep -v 'grep' |grep "+dom.name()+"|awk '{print $2}' `/status").readlines()[0][:-1]
-						memusage=int(memstatus)*100.0/int(dom_info[2])
+						memusage='%.2f' %  (int(memstatus)*100.0/int(dom_info[2]))
 						if memusage>100:
 							memusage=100
-						name=int(dom.name().split('-')[-1],16)
+						name=ElementTree.fromstring(dom.XMLDesc(0)).findtext('uuid')
 					except (KeyboardInterrupt,SystemExit):
 						raise
 					except:
@@ -88,19 +83,15 @@ def get_node_info():
 					cmd="insert into instance (instance_id,timestamp,cpu,mem,disk,net)  values ('"+str(name)+"','"+str(time.time())+"','"+str(cpu_usage)[:5]+"','"+str(memusage)+"','"+block_status+"','"+nic_status+"')"
 					print cmd
 					cursor.execute(cmd)
-					db_conn.close()	
+					db_conn.commit()
+					db_conn.close()
 					queue.task_done()
-	
+
 	if queue.qsize()<MAXTHREAD:
 		MAXTHREAD=queue.qsize()
 	for i in xrange(0,MAXTHREAD):
 		get_dom_info().start()
 	queue.join()
-def start():
-	while True:
-		get_node_info()
-		time.sleep(300)
 
 if __name__=="__main__":
-	daemonize(stdout=log_dir+'/instancestdout.log', stderr=log_dir+'/instancestderr.log',pidfile=log_dir+'/monitor/pid.txt')
-	start()
+	get_node_info()
